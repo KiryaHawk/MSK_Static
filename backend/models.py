bac327fc66f221cc9f06e60781f1ae4ceb58a77b
KiryaHawk <kiryahawk@gmail.com>
2025-07-15 15:13:50 +0700

    Локальные изменения

--- backend/models.py
+++ backend/models.py
@@ -8,3 +8,11 @@ class Point(Base):
     name = Column(String, index=True)
     latitude = Column(String)
     longitude = Column(String)
+    
+class CommentColor(Base):
+    __tablename__ = "comments_colors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    object_id = Column(String, unique=True, index=True)
+    comment = Column(String)
+    color = Column(String)
